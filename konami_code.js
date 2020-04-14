857ab106a2c4633ec5fd0c1c77bb753c484cd174
Ian McKenzie <imckenzie777@gmail.com>
2020-04-14 23:29:49 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,31 +12,36 @@ const codes = [
 ];
 
 function init() {
+  let count = 0;
   document.body.addEventListener("keydown", (event) => {
     console.log(event.key)
-    if (event.defaultPrevented) {
-      return; // Do nothing if event already handled Have to understand this.
+
+
+    if (event.key === codes[count]) {
+      count++
+    } else {
+      count = 0;
     }
-    switch(event.code) {
-    	case "KeyA":
-    	case "ArrowUp": // Handle "up arrow"
-    	break;
-    	case "KeyS":
-    	case "ArrowDown": // Handle "down arrow"
-    	break;
-    	case "KeyD":
-    	case "ArrowLeft": // Handle "left arrow"
-    	break;
-    	case "KeyF":
-    	case "ArrowRight": // Handle "left arrow"
-    	break;
-      case "KeyG":
-      case "a": // Handle 'a' button
-      break;
-      case "KeyH":
-      case "b": // Handle 'b' button
-      break;
-    }
+    // switch(event.code) {
+    // 	case "KeyA":
+    // 	case "ArrowUp": // Handle "up arrow"
+    // 	break;
+    // 	case "KeyS":
+    // 	case "ArrowDown": // Handle "down arrow"
+    // 	break;
+    // 	case "KeyD":
+    // 	case "ArrowLeft": // Handle "left arrow"
+    // 	break;
+    // 	case "KeyF":
+    // 	case "ArrowRight": // Handle "left arrow"
+    // 	break;
+    //   case "KeyG":
+    //   case "a": // Handle 'a' button
+    //   break;
+      // case "KeyH":
+    //   case "b": // Handle 'b' button
+    //   break;
+    // }
     // Consume the event so it doesn't get handled twice
     event.preventDefault();
 
