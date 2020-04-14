362a8a8701e139d0007af5fd075790b76a987be2
Ian McKenzie <imckenzie777@gmail.com>
2020-04-14 22:38:05 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,32 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  document.body.addEventListener("keydown", (event) => {
+    // Now, how can we check for which specific key was pressed?
+    if (event.defaultPrevented) {
+      return; // Do nothing if event already handled Have to understand this.
+    }
+    switch(event.code) {
+    	case "KeyA":
+    	case "ArrowUp":
+    	// Handle "back"
+    	// updatePosition(-moveRate);
+    	break;
+    	case "KeyS":
+    	case "ArrowDown":
+    	// Handle "forward"
+    	// updatePosition(moveRate);
+    	break;
+    	case "KeyD":
+    	case "ArrowLeft":
+    	// Handle "turn left"
+    	/*angle -= turnRate;*/
+    	break;
+    	case "KeyF":
+    	case "ArrowRight":
+    	// Handle "turn right"
+    	/*angle += turnRate;*/
+    	break;
+    }
+  });
 }
